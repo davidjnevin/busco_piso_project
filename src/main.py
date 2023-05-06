e604223377e1611dc0e5500850e529b18d9fe28e
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-05-06 23:37:55 +0200

    Fix: Move the health-check api to api_routes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
+from src.api_routers.health_check import router as health_check_router
+
 
 app = FastAPI()
+app.include_router(health_check_router)
 
-@app.get("/health-check")
-def health_check():
-    return {"status": "ok"}
